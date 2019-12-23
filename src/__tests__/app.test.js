d9de78bc81716db4e7b5c96cd4428f2cb6361e90
Matt Johnson <majikjohnson@hotmail.com>
2019-12-23 22:41:29 +0000

    Able to calculate total time from pace and distance

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import app from '../app';
+import 'chai/register-should';
 
+/*
 test('It passes', async () => {
 	const res = await request(app)
 		.post('/paceCalculator')
@@ -10,4 +12,55 @@ test('It passes', async () => {
 		});
 	expect(res.statusCode).toEqual(200);
 	expect(res.body).toHaveProperty('post');
+});
+*/
+
+describe('Calculate Time Module', () => {
+	it('calculates correct time for pace in miles and distance in miles', async () => {
+		const res = await request(app)
+			.post('/paceCalculator')
+			.send({
+				calculation: 'time',
+				input: {
+					distance: {
+						length: 26.21875,
+						unit: 'miles',
+					},
+					pace: {
+						hours: 0,
+						minutes: 7,
+						seconds: 0,
+						unit: 'miles',
+					},
+				},
+			});
+		expect(res.statusCode).toEqual(200);
+		res.body.time.hours.should.be.equal(3);
+		res.body.time.minutes.should.be.equal(3);
+		res.body.time.seconds.should.be.equal(31.875);
+	});
+
+	it('calculates correct time for pace in kilometers and distance in miles', async () => {
+		const res = await request(app)
+			.post('/paceCalculator')
+			.send({
+				calculation: 'time',
+				input: {
+					distance: {
+						length: 26.21875,
+						unit: 'miles',
+					},
+					pace: {
+						hours: 0,
+						minutes: 4,
+						seconds: 20,
+						unit: 'kilometers',
+					},
+				},
+			});
+		expect(res.statusCode).toEqual(200);
+		res.body.time.hours.should.be.equal(3);
+		res.body.time.minutes.should.be.equal(2);
+		res.body.time.seconds.should.be.equal(50.69688000000133);
+	});
 });
--- src/app.js
+++ src/app.js
@@ -6,7 +6,34 @@ const app = express();
 app.use(express.json());
 
 app.post('/paceCalculator', (req, res) => {
-	res.send(req.body);
+	switch (req.body.calculation) {
+		case 'time':
+			const hoursPace = req.body.input.pace.hours;
+			const minutesPace = req.body.input.pace.minutes;
+			const secondsPace = req.body.input.pace.seconds;
+			const paceUnit = req.body.input.pace.unit;
+			const distance = req.body.input.distance.length;
+			
+			const totalSecondsPace = (hoursPace * 3600) + (minutesPace * 60) + secondsPace;
+			const totalSecondsTime = paceUnit === 'miles' ? distance * totalSecondsPace : distance * totalSecondsPace * 1.609344;
+
+			const hours = Math.floor(totalSecondsTime / 3600);
+			const mins = Math.floor((totalSecondsTime % 3600) / 60);
+			const secs = totalSecondsTime % 60;
+
+			res.json({
+				time: {
+					hours: hours,
+					minutes: mins,
+					seconds: secs
+				},
+			});
+			break;
+	}
+
+	res.status(500).json({
+		msg: "Couldn't understand the request",
+	});
 });
 
 export default app;
