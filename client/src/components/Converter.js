bb45b7ce957fd0df0dd01608276310a33982a67b
Matt Johnson <majikjohnson@hotmail.com>
2019-12-28 23:32:13 +0000

    Added expansion panels to Converter component

--- client/src/components/Converter.js
+++ client/src/components/Converter.js
@@ -1,20 +1,39 @@
 import React from 'react';
 
 //Material UI imports
-import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+	root: {
+		marginTop: '16px',
+	},
+});
 
 const Converter = () => {
+    const classes = useStyles();
 	return (
-		<Container>
+		<div className={classes.root}>
 			<ExpansionPanel>
-				<ExpansionPanelSummary>Time</ExpansionPanelSummary>
+				<ExpansionPanelSummary>
+                    <Typography variant="h6">
+                        Time
+                    </Typography>
+                </ExpansionPanelSummary>
 				<ExpansionPanelDetails>Enter time</ExpansionPanelDetails>
 			</ExpansionPanel>
-		</Container>
+            <ExpansionPanel>
+				<ExpansionPanelSummary>
+                    <Typography variant="h6">
+                        Distance
+                    </Typography>
+                </ExpansionPanelSummary>
+				<ExpansionPanelDetails>Enter distance</ExpansionPanelDetails>
+			</ExpansionPanel>
+		</div>
 	);
 };
 
