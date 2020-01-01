a3e3a94d589ace9efcfc74223a7b474ecd58480d
Matt Johnson <majikjohnson@hotmail.com>
2020-01-01 23:12:57 +0000

    Rewrote Distance control using grid for better formatting

--- client/src/components/DistancePanel.js
+++ client/src/components/DistancePanel.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Typography from '@material-ui/core/Typography';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
+import { Grid, InputLabel, Select } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-	formControl: {
-		margin: theme.spacing(1),
-		minWidth: 120,
+	root: {
+		flexGrow: 1,
 	},
-	selectEmpty: {
-		marginTop: theme.spacing(2),
+	header: {
+		backgroundColor: theme.palette.primary.main,
+		color: 'white',
+		padding: theme.spacing(2),
 	},
+	body: {
+		backgroundColor: theme.palette.primary.light,
+	},
+	distanceControl: {
+		padding: theme.spacing(2),
+	},
+	unitControl: {
+		paddingTop: theme.spacing(2),
+		paddingBottom: theme.spacing(2),
+		paddingRight: theme.spacing(2),
+	},
+	raceControl: {
+		paddingLeft: theme.spacing(2),
+		paddingRight: theme.spacing(2),
+		paddingBottom: theme.spacing(2),
+	}
 }));
 
 const units = [
@@ -39,44 +54,73 @@ const DistancePanel = () => {
 	};
 
 	return (
-		<ExpansionPanel expanded={true}>
-			<ExpansionPanelSummary>
-				<Typography variant="h6">Distance</Typography>
-			</ExpansionPanelSummary>
-			<ExpansionPanelDetails>
-				<TextField
-					id="distance"
-					label="Enter distance"
-					variant="filled"
-					margin="normal"
-					size="small"
-					type="number"
-				/>
-				<FormControl className={classes.formControl}>
-					<TextField
-						id="unit"
-						select
-						label='unit'
-						variant="filled"
-						margin="dense"
-						InputLabelProps={{
-							strink: 'false'
-						}}
-						SelectProps={{
-							native: true,
-						}}
-						value={unit}
-						onChange={handleChange}
-					>
-						{units.map(option => (
-							<option key={option.value} value={option.value}>
-								{option.label}
-							</option>
-						))}
-					</TextField>
-				</FormControl>
-			</ExpansionPanelDetails>
-		</ExpansionPanel>
+		<div className={classes.root}>
+			<Grid container className={classes.header}>
+				<Grid item>
+					<Typography variant="h6">Distance</Typography>
+				</Grid>
+			</Grid>
+			<Grid container className={classes.body}>
+				<Grid item xs={7} className={classes.distanceControl}>
+					<FormControl>
+						<TextField
+							id="distance"
+							label="Enter distance"
+							variant="filled"
+							type="number"
+						/>
+					</FormControl>
+				</Grid>
+				<Grid item xs={5} className={classes.unitControl}>
+					<FormControl fullWidth>
+						<TextField
+							id="unit"
+							select
+							label="unit"
+							variant="filled"
+							InputLabelProps={{
+								strink: 'false',
+							}}
+							SelectProps={{
+								native: true,
+							}}
+							value={unit}
+							onChange={handleChange}
+						>
+							{units.map(option => (
+								<option key={option.value} value={option.value}>
+									{option.label}
+								</option>
+							))}
+						</TextField>
+					</FormControl>
+				</Grid>
+				<Grid item xs={12} className={classes.raceControl}>
+					<FormControl fullWidth>
+						<TextField
+							id="unit"
+							select
+							label="unit"
+							variant="filled"
+							InputLabelProps={{
+								strink: 'false',
+							}}
+							SelectProps={{
+								native: true,
+							}}
+							value={unit}
+							onChange={handleChange}
+						>
+							{units.map(option => (
+								<option key={option.value} value={option.value}>
+									{option.label}
+								</option>
+							))}
+						</TextField>
+					</FormControl>
+				</Grid>
+			</Grid>
+		</div>
 	);
 };
 
